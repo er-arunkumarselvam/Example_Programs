7c8e70586486467bc21bcdaac651e4d7f4c51de0
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2022-10-14 20:24:27 +0530

    Learn two class file accesssed the methods - GrandMall.java file access the class in Viewer.java

---
+++ Viewer.java
@@ -0,0 +1,14 @@
+class Viewer
+{
+public static void main(String[] args)
+{
+GrandMall manager = new GrandMall(); 
+manager.show("Ponniyin Selvan"); 
+System.out.println(manager.ticket_price);
+//manager.get_profit(); 
+System.out.println(manager.salary);
+
+}
+
+
+}
